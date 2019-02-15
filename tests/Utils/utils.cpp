fe54470c276f7e4ea4d5aa597be015a48715e02c
René Hansen <renehh@gmail.com>
2019-02-15 14:38:59 +0100

    Updates test Utils::migrateAsset

--- tests/Utils/utils.cpp
+++ tests/Utils/utils.cpp
@@ -20,13 +20,35 @@ QString Utils::migrateAsset(QString path)
   QString output_file_dir = output_file_path_copy.remove(output_file_info.fileName());
   QFile input_file(path);
 
+  qDebug() << "UTILS migrateAsset" << input_file << output_file_path;
   if (QDir().mkpath(output_file_dir))
   {
-    if (!input_file.copy(output_file_path)) {
-      qDebug() << path << "not copied, since" << output_file_path << "already exists.";
-    }
+    if (output_file_info.exists())
+    {
+      qDebug() << "File" << output_file_path << "already exists. Not migrated.";
+      //qDebug() << "Content of" << output_file_dir << ":";
 
-    return output_file_path;
+      //QDir dir(appDataLocation);
+      //dir.setFilter(QDir::AllEntries | QDir::NoDotAndDotDot);
+      //QFileInfoList list = dir.entryInfoList();
+      //qDebug() << "     Bytes Filename";
+      //for (int i = 0; i < list.size(); ++i) {
+      //  QFileInfo fileInfo = list.at(i);
+      //  qDebug() << QString("%1 %2").arg(fileInfo.size(), 10).arg(fileInfo.fileName());
+      //}
+
+      return output_file_path;
+    }
+    else
+    {
+      if (input_file.copy(output_file_path)) {
+        return output_file_path;
+      }
+      else
+      {
+        qDebug() << "Failed to copy" << path;
+      }
+    }
   }
   else
   {
